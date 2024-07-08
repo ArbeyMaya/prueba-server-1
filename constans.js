3a9468b76a0fa5e7f4c38fe6aaa7f872cd5da484
Arbey Maya <vilmarbey@gmail.com>
2024-07-08 07:03:42 -0500

    first commit

---
+++ constans.js
@@ -0,0 +1,15 @@
+const DB_USER = "Arbey"
+const DB_PASSWORD = "Arbeygamer123"
+const DB_HOST = "@cluster0.fsnpfmv.mongodb.net" 
+
+const API_VERSION = "v1"
+const IP_SERVER ="lovcalhost"
+
+module.exports ={
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER
+}
+
